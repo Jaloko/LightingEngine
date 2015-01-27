b6f4965bcc02fd34ea98102c4d7e251b6fc36403
Jaloko <jaloko@hotmail.co.nz>
2015-01-27 16:04:51 +1300

    Updated examples, added index page for all examples

---
+++ examples/js/index.js
@@ -0,0 +1,12 @@
+function resize() {
+    var content = document.getElementById("content");
+    content.style.width = window.innerWidth - 250 + "px";
+}
+
+function changeExample(url, element) {
+    var iframe = document.getElementById("iframe").src = url;
+    for(var i = 1; i <= 3; i++) {
+        document.getElementById("example" + i).className = "content-cell";
+    }
+    document.getElementById(element + "").className = "content-cell selected";
+}
